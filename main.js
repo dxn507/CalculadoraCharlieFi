01e54dd6621f5a50c461d9bc335428cef9eace2e
Carlos Rivera <81501241+dxn507@users.noreply.github.com>
2025-03-01 16:51:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const pantalla =document.getElementById("pantalla");
+const pantalla =document.getElementById('pantalla');
 const botonesNumeros = document.querySelectorAll('.numero');
 const btnClear=document.getElementById('botonClar');
 const suma=document.getElementById('suma');
 const resta=document.getElementById('sustraccion');
-const divicion =document.getElementById("divicion");
-const multiplicacion=document.getElementById("multiplicacion");
+const divicion =document.getElementById('divicion');
+const multiplicacion=document.getElementById('multiplicacion');
 const botonigual =document.getElementById('igual');
-
+//reset inicial
 var cifra =""; //num2
 var acumulado =0; //num1
 var sumar = false;
@@ -21,7 +21,7 @@ botonesNumeros.forEach(boton =>{
        // pantalla+=boton.value
        pantalla.value=cifra+boton.value;  //lo que se ve en pantalla
        cifra = pantalla.value; //se actualiza cifra
-    })
+    });
 });
 
 //SUMA
@@ -39,7 +39,7 @@ suma.addEventListener('click',()=>{
       acumulado=acumulado+parseInt(cifra); //actualiza acumulado
       pantalla.value=acumulado;
       }
-      cifra =" "; //resetea cifra
+      cifra =""; //resetea cifra
       sumar =true;
       restar=false;
       dividir=false
@@ -75,25 +75,33 @@ resta.addEventListener('click', ()=>{
 
 //Division
 divicion.addEventListener('click', ()=>{
-   if(sumar){
+  if(sumar){
     acumulado=acumulado+parseInt(cifra);
     pantalla.value=acumulado;
-    }else if(restar){
-      acumulado=acumulado-parseInt(cifra);
-      pantalla.value=acumulado;
-    }else if(multiplicar){
+   }else if(restar){
+    acumulado=acumulado-parseInt(cifra);
+    pantalla.value=acumulado;
+  }else if(multiplicar){
       acumulado=acumulado*parseInt(cifra);
       pantalla.value=acumulado;
-    }else{
-    acumulado=acumulado/parseInt(cifra); //actualiza acumulado
-    pantalla.value=acumulado;
+  }else if(dividir){
+     acumulado=acumulado/parseInt(cifra);
+      pantalla.value=acumulado;
+  }else{
+    
+    acumulado=acumulado+parseInt(cifra); //actualiza acumulado
+    pantalla.value=cifra;
+    
     }
-  cifra =" "; //resetea cifra
+
+  cifra =""; //resetea cifra
   sumar=false;
   restar=false
-  dividir = true;
   multiplicar=false;
+  dividir=true;
+  p_operacion = false;
 });
+
 //multiplicar
 multiplicacion.addEventListener('click', ()=>{
   if(sumar){
@@ -103,17 +111,22 @@ multiplicacion.addEventListener('click', ()=>{
       acumulado=acumulado-parseInt(cifra);
       pantalla.value=acumulado;
   }else if(dividir){
-    acumulado=acumulado/parseInt(cifra); //actualiza acumulado
+    acumulado=acumulado/parseInt(cifra); 
     pantalla.value=acumulado;
-  }else{
+  }else if(multiplicar){
+    acumulado=acumulado*parseInt(cifra);
+      pantalla.value=acumulado;
+  }
+  else{
     acumulado=acumulado+parseInt(cifra); //actualiza acumulado
-    cifra=pantalla.value
+    pantalla.value=cifra;
   }
   cifra =""; //resetea cifra
   sumar=false;
-  restar=false
+  restar=false;
   dividir = false;
   multiplicar=true;
+  p_operacion = false;
 });
 
 function resultado() {
@@ -124,17 +137,22 @@ function resultado() {
         pantalla.value=acumulado-parseInt(cifra);
     }else if(dividir){
       pantalla.value=acumulado/parseInt(cifra);
+      dividir=true;
     }else if(multiplicar){
       pantalla.value=acumulado*parseInt(cifra);
+      multiplicar=true;
     }
-      acumulado=parseInt(pantalla.value);
-      cifra=0;
+    
 }
 function clear(){
-    pantalla.value=0;
+  pantalla.value=0;
     cifra="";
     acumulado=0;
     p_operacion = true;
+    sumar = false;
+    restar = false;
+    dividir=false;
+    multiplicar=false;
 }
 
 botonigual.addEventListener('click',resultado);
